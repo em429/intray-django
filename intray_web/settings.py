28a7c700f94a53235d656655f4e864ad2160acdc
ajz <mate@lamnd.ax>
2021-02-11 20:52:05 +0100

    update settings for nixos

--- intray_web/settings.py
+++ intray_web/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "intray_web.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, os.environ.get("DB_NAME")),
+        "NAME": os.environ.get("DB_NAME"),
     }
 }
 
